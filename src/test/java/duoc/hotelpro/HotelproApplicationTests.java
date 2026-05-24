8a11f0ecb8fb485da5101237c99e32c6db9bc953
slowbrk <b.ampuero@duocuc.cl>
2026-05-24 18:34:41 -0400

    Cuerpo de proyecto

---
+++ src/test/java/duoc/hotelpro/HotelproApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.hotelpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
