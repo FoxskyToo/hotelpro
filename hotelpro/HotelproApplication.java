c0591ee271c3e9db20a5cc286d9fedeff613ada8
FoxskyToo <bo.perezb@duocuc.cl>
2026-05-24 18:26:51 -0400

    Add files via upload

---
+++ hotelpro/HotelproApplication.java
@@ -0,0 +1,13 @@
+package duoc.hotelpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelproApplication.class, args);
+	}
+
+}
